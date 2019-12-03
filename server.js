9df6cf0f344f31d4012c2e4a3f6026337df1864f
Wali <wali.rahman89@yahoo.com>
2019-12-02 20:11:46 -0500

    added simple crud schema

---
+++ server.js
@@ -0,0 +1,24 @@
+var { graphql, buildSchema } = require('graphql');
+
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+  type Query {
+    hello: String
+    bye: String
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+var root = {
+  hello: () => {
+    return 'Hello world!';
+  },
+  bye: () => {
+    return 'Bye world!';
+  },
+};
+
+// Run the GraphQL query '{ hello }' and print out the response
+graphql(schema, '{ bye }', root).then((response) => {
+  console.log(response);
+});
