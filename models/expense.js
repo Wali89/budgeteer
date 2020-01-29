d49779a43fdaa951f3a62845c5170b79e40bef64
Wali <wali.rahman89@yahoo.com>
2020-01-28 19:32:16 -0500

    updated expense model

--- models/expense.js
+++ models/expense.js
@@ -4,5 +4,5 @@ const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/
 const client = new pg.Client(connectionString);
 client.connect();
 const query = client.query(
-  'CREATE TABLE expenses(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, date DATE NOT NULL DEFAULT CURRENT_DATE, category_id INT NOT NULL REFRENCES category (id))');
+  'CREATE TABLE expenses(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, date DATE NOT NULL DEFAULT CURRENT_DATE, amount INT NOT NULL, category_id INT NOT NULL REFRENCES category (id))');
 query.on('end', () => { client.end(); });
